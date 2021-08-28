0c8f9705ed15c94ad40d9db5c6ea5690d7703f80
devdeadviz <kuldeepgupta485@gmail.com>
2021-08-28 22:24:15 +0530

    adding functionality

--- src/App.js
+++ src/App.js
@@ -1,37 +1,68 @@
 import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [orgValue, setOrgValue] = useState("");
+  const [repoValue, setRepoValue] = useState("");
+  const [activeIssues, setActiveIssues] = useState("");
+
+  //GitHub API
+  var url = "https://api.github.com/repos/organisation/issues";
+
+  function addText(org, repo) {
+    return url.replace("organisation", org + "/" + repo);
+  }
+
+  async function clickHandler() {
+    var url = addText(orgValue, repoValue);
+
+    let counter = 0;
+    await fetch(url)
+      .then((response) => response.json())
+      .then((json) =>
+        json.forEach((elements) => {
+          if (elements.state === "open") {
+            counter = Number(counter) + 1;
+          }
+        })
+      );
+    setActiveIssues(counter);
+  }
+
   return (
-    <div className="App">
-      <h1> Search Your Github Issues </h1>
-      <div className="wrapper">
-        <div className="title">
-          Search Github Issues 
+    <div className="wrapper">
+      <div className="title">Search Github Issues</div>
+      <form action="#">
+        <div className="field">
+          <input
+            type="text"
+            onChange={(e) => setOrgValue(e.target.value)}
+            required
+          ></input>
+          <label>Organisation</label>
         </div>
-        < form action='#'>
-          <div className="field">
-            <input type='text'required></input>
-            <label> Profile</label>
-          </div>
-          <div className="field">
-            <input type='text' required>
-            </input>
-            <label> Issue</label>
-          </div>
-          <div class="container">
-                <div class="center">
-                     <button className="bt" type="button">Submit</button>
-                </div>
-                </div>
-        </form>
-        <div className="containerSecond">
-          <div className="centerSecond">
-            <h2> Output</h2>
+        <div className="field">
+          <input
+            type="text"
+            onChange={(e) => setRepoValue(e.target.value)}
+            required
+          ></input>
+          <label>Repository</label>
+        </div>
+        <div class="container">
+          <div class="center">
+            <button className="bt" type="button" onClick={clickHandler}>
+              Submit
+            </button>
           </div>
         </div>
-        
+      </form>
+      <div className="containerSecond">
+        <div className="centerSecond">
+          <h2>Active Issues are:</h2>
+          {activeIssues}
+        </div>
       </div>
-      
     </div>
   );
 }
