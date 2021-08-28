6b189de754979f0ef9ae517325827273769f8184
Harshit Joshi <harshitjoshi250@gmail.com>
2021-08-28 18:45:47 +0530

    commit in app.css and app.js

--- src/App.js
+++ src/App.js
@@ -3,11 +3,35 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <h1>Search Github Issues</h1>
-      <input />
-      <input />
-      <button>Submit</button>
-      <div></div>
+      <h1> Search Your Github Issues </h1>
+      <div className="wrapper">
+        <div className="title">
+          Search Github Issues 
+        </div>
+        < form action='#'>
+          <div className="field">
+            <input type='text'required></input>
+            <label> Profile</label>
+          </div>
+          <div className="field">
+            <input type='text' required>
+            </input>
+            <label> Issue</label>
+          </div>
+          <div class="container">
+                <div class="center">
+                     <button className="bt" type="button">Submit</button>
+                </div>
+                </div>
+        </form>
+        <div className="containerSecond">
+          <div className="centerSecond">
+            <h2> Output</h2>
+          </div>
+        </div>
+        
+      </div>
+      
     </div>
   );
 }
